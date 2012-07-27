911f753da601be8012b726861779fc9206823877
Julien Glotain <jglotain@gmail.com>
2012-07-27 17:15:42 +0200

    Add spec test on lastDayOfMonth function

---
+++ test/moment.prototype.specs.js
@@ -0,0 +1,27 @@
+describe("Moment.js Prototypes functions",function(){
+	var now, Month = {};
+
+	now = moment();
+	Month.FEBRUARY = 1;
+
+	// add new matchers
+	beforeEach(function() {
+	  this.addMatchers({
+	    toBeTheSameDay: function(expected) {
+	      return this.actual.equals(expected);
+	    }
+	  });
+	});
+
+	describe("A function should return last day of a specific month",function(){
+		it("should get last day of feb 2012",function(){
+
+			// expected date is 29 feb 2012
+			var expected = moment().date(28).month(Month.FEBRUARY).year(2012);
+			var result = moment().date(1).month(Month.FEBRUARY).year(2012).lastDayOfMonth();
+			
+			expect(result).toBeTheSameDay(expected);
+		});
+	});
+	
+});
