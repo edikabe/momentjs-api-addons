ff162cf340537a1cd7fa90454c568f3a43ffabb8
Julien GLOTAIN <jglotain@gmail.com>
2015-05-26 23:20:41 +0200

    Modified spec as Jasmine version was upgraded

--- src/moment.addons.js
+++ src/moment.addons.js
@@ -8,34 +8,34 @@
 	 */
 	moment.fn.lastDayOfMonth = function(){
 		return this.add('months', 1).date(0);
-	};
+	}
 
 	/**
 	 * Tests if this date is after the specified date.
 	 */
 	moment.fn.before = function(comparedDate){
 		return this.diff(comparedDate) < 0;
-	};
+	}
 
 	/**
 	 * Tests if this date is after the specified date.
 	 */
 	moment.fn.after = function(comparedDate){
 		return this.diff(comparedDate) > 0;
-	};
+	}
 
 	/**
 	 * Compares two dates for equality.
 	 */
 	moment.fn.equals = function(comparedDate){
 		return this.diff(comparedDate) === 0;
-	};
+	}
 
 	/**
-	 * Is the date between a range of dates(inclusive). 
+	 * Is the date between a range of dates(inclusive).
 	 */
 	moment.fn.between = function(start,end){
 		return ((this.equals(start)||this.equals(end)) || (this.after(start) && this.before(end))) ? true : false;
-	};
+	}
 
 })(moment);
--- test/MomentAddonsSpec.js
+++ test/MomentAddonsSpec.js
@@ -1,30 +1,29 @@
-describe("Moment.js Prototypes new functions",function(){
+describe("Moment.js API addons",function(){
 	var now, Month = {};
 
 	now = moment();
 	Month.FEBRUARY = 1;
 
 	// add new matchers
-	beforeEach(function() {
-	  this.addMatchers({
-	    toBeTheSameDay: function(expected) {
-	      return this.actual.equals(expected);
-	    }
-	  });
-	});
+	beforeEach(function(){});
 
 	describe("A function should return last day of a specific month",function(){
 		it("should get last day of feb 2012",function(){
 
 			// expected date is 29 feb 2012
-			var expected = moment().date(29).month(Month.FEBRUARY).year(2012);
+			var expected = moment("29-02-2012","DD-MM-YYYY");
+
+			// given
 			var result = moment().date(1).month(Month.FEBRUARY).year(2012).lastDayOfMonth();
-			
-			expect(result).toBeTheSameDay(expected);
+
+			// then
+			expect(result.date()).toBe(expected.date());
+			expect(result.month()).toBe(expected.month());
+
 		});
 	});
 
-	// ToDo write specs for 
+	// ToDo write specs for
 	//  before, after, equals and between methods
-		
+
 });
